196bbeb64b26589148eb5a01f294571d867f3b07
Ivo van der Linden <hello@imivo.dev>
2023-09-19 17:11:38 +0200

    Update | 1.1.0

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,7 +5,7 @@ lua54 "yes"
 
 name "ay-playtime"
 author "Array's Development | Array"
-version "1.0.0"
+version "1.1.0"
 description "Playtime script made for FiveM server which uses playtime based interactions"
 
 shared_scripts { '@es_extended/imports.lua' }
